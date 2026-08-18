704b30b54e0919e01ce3ea79c1180a068ea9a9b8
sheiza31 <seizzafr@gmail.com>
2026-08-18 21:43:21 +0700

    first commit

--- frontend/backend/requests/category_request.go
+++
@@ -1,7 +0,0 @@
-package requests
-
-type CategoryCreateRequest struct {
-	Name        string `form:"name" binding:"required" json:"name"`
-	Slug        string `form:"slug" binding:"required" json:"slug"`
-	Description string `form:"description" binding:"required" json:"description"`
-}

231f5a985a04bbd0f7040f6c78d9e873640e9844
sheiza31 <seizzafr@gmail.com>
2026-08-18 21:28:02 +0700

    remove duplicated root files, keep in frontend/

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import FormLogin from "./components/FormLogin";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center px-stack-md">
-      <FormLogin />
-    </div>
-  );
-}
